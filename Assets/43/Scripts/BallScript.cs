1701f46fe02028530b24959ae0a28148cf9dd432
shupopo-999 <shu999.20099@gmail.com>
2024-10-13 02:36:21 +0900

    43回次完成

---
+++ Assets/43/Scripts/BallScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallScript : MonoBehaviour
+{
+	public int score;
+	public int z_score;
+
+	public Text text;
+	public Text T_text;
+	// Use this for initialization
+	void Start()
+    {
+		z_score = PlayerPrefs.GetInt("zenkai",0);
+		T_text.text = "前回のスコア:" + z_score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		text.text = "あなたのスコア:" + score;
+	}
+
+    void OnCollisionEnter(Collision col)
+	{
+		score++;
+		if (col.gameObject.name == "stopper")
+		{
+			PlayerPrefs.SetInt("zenkai",score);
+		}
+        
+    }
+}
---
+++ Assets/43/Scripts/Player_43.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//プレイヤー用のスクリプト。さわらないでね！
+public class Player_43 : MonoBehaviour {
+
+	public FixedJoint fj;
+	public Rigidbody rb;
+	bool hasshaShita;
+	// Use this for initialization
+	void Start () {
+		hasshaShita = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			//Aキーで左に、Dキーで右にうごく
+		if (Input.GetKey (KeyCode.D) && transform.position.x < 12f) {
+				transform.Translate (0.4f, 0f, 0f);
+			}
+		if (Input.GetKey (KeyCode.A) && transform.position.x > -12f) {
+				transform.Translate (-0.4f, 0f, 0f);
+			}
+		
+		//スペースキーではっしゃ
+		if (Input.GetKeyDown (KeyCode.Space) && hasshaShita == false) {
+			hasshaShita = true;
+			fj.breakForce = 0f;
+			rb.AddForce (0f,-2300f,0f);
+		}
+			
+	}
+}
