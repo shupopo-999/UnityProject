1885c1804b372916fb11098a37145adcb00178c3
shu-999 <syu.inui@gmail.com>
2024-12-30 02:08:33 +0900

    p

---
+++ Assets/61/copy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copy : MonoBehaviour
+{
+    float t;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = Time.fixedTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.fixedTime - t > 0.2f)
+        {
+            create();
+            t = Time.fixedTime;
+        }
+    }
+    public void create()
+    {
+        //この部分を書いてみよう！！！
+        Instantiate()
+    }
+}
