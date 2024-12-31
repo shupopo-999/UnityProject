34a33e3420525496e4db1fed1fd381ad9a9cdfe8
shu-999 <syu.inui@gmail.com>
2025-01-01 03:04:09 +0900

    セカンド終わり
    63回次終わり

---
+++ Assets/62/src/batter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class batter : MonoBehaviour
+{
+    public GameObject player;
+    public Text tex;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Ball") {
+            float byou = Random.Range(0.2f, 1.5f);
+            StartCoroutine("Swing", byou);
+            tex.text = "Hit!!";
+        }
+    }
+
+    IEnumerator Swing(float byou) {
+        // player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.Euler(0f, -130f, 0f), 1f);
+        anim.Play("batting");
+        yield return new WaitForSeconds(byou);
+    }
+}
---
+++ Assets/62/src/batting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batting : MonoBehaviour
+{
+    public GameObject player;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Ball") {
+            float byou = Random.Range(0.3f, 1f);
+            StartCoroutine("Swing", byou);
+        }
+    }
+
+    IEnumerator Swing(float byou) {
+        // player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.Euler(0f, -130f, 0f), 1f);
+        anim.Play("batting");
+        yield return new WaitForSeconds(byou);
+    }
+}
